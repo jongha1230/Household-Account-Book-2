862ff6ea3fa5f38ebab3704ebe9514b236960b11
Jongha Lim <jongha1230@gmail.com>
2024-06-13 12:16:42 +0900

    사용 안하는 주석 제거

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,24 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../slices/authSlice";
-// import fetchedDataReducer from "../slices/fetchedDataSlice";
 import modalReducer from "../slices/modalSlice";
-
-const localStorageMiddleware = (store) => (next) => (action) => {
-  const result = next(action);
-  const state = store.getState();
-
-  localStorage.setItem("dataItem", JSON.stringify(state.fetchedData));
-  return result;
-};
 
 const store = configureStore({
   reducer: {
-    // fetchedData: fetchedDataReducer,
     modal: modalReducer,
     auth: authReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(localStorageMiddleware),
 });
 
 export default store;
